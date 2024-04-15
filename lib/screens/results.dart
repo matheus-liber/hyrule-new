78cc58c8c60e6dc8ab5c72d355acd98a1e793076
matheus-liber <matheuslibergt@gmail.com>
2024-04-15 14:55:11 -0300

    Transition of category display

--- lib/screens/results.dart
+++ lib/screens/results.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:hyrule/controllers/api_controller.dart';
 import 'package:hyrule/domain/models/entry.dart';
@@ -7,7 +8,8 @@ import 'package:hyrule/screens/favorites.dart';
 import 'package:hyrule/utils/consts/categories.dart';
 
 class Results extends StatefulWidget {
-const Results({ Key? key, required this.category, required this.entries }) : super(key: key);
+  const Results({Key? key, required this.category, required this.entries})
+      : super(key: key);
   final String category;
   final List<Entry> entries;
 
@@ -21,31 +23,38 @@ class _ResultsState extends State<Results> {
   bool cardView = true;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(categories[widget.category]!),
-          actions: [
-            IconButton(onPressed: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Favorites(),
-                      ));
-                },
-                icon: const Icon(Icons.bookmark)),
-            IconButton(
-                onPressed: () => setState(() {
-                      cardView = !cardView;
-                    }),
-                icon: const Icon(Icons.swap_horiz))
-          ],
-        ),
-        body: cardView
-            ? _CardList(entries: widget.entries)
-            : _ListView(entries: widget.entries),
-      ),
+          appBar: AppBar(
+            title: Text(categories[widget.category]!),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Favorites(),
+                        ));
+                  },
+                  icon: const Icon(Icons.bookmark)),
+              IconButton(
+                  onPressed: () => setState(() {
+                        cardView = !cardView;
+                      }),
+                  icon: const Icon(Icons.swap_horiz))
+            ],
+          ),
+          body: PageTransitionSwitcher(
+            transitionBuilder: ((child, primaryAnimation, secondaryAnimation) =>
+                FadeThroughTransition(
+                    animation: primaryAnimation,
+                    secondaryAnimation: secondaryAnimation,
+                    child: child)),
+            child: cardView
+                ? _CardList(entries: widget.entries)
+                : _ListView(entries: widget.entries),
+          )),
     );
   }
 }
@@ -57,7 +66,13 @@ class _CardList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) => EntryCard(entry: entries[index], isSaved: false,), itemCount: entries.length,);
+    return ListView.builder(
+      itemBuilder: (context, index) => EntryCard(
+        entry: entries[index],
+        isSaved: false,
+      ),
+      itemCount: entries.length,
+    );
   }
 }
 
@@ -68,7 +83,10 @@ class _ListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) => _ListTile(entry: entries[index]), itemCount: entries.length,);
+    return ListView.builder(
+      itemBuilder: (context, index) => _ListTile(entry: entries[index]),
+      itemCount: entries.length,
+    );
   }
 }
 
@@ -80,7 +98,8 @@ class _ListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      contentPadding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      contentPadding:
+          const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       leading: Container(
           clipBehavior: Clip.hardEdge,
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(4.0)),
